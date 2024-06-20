8dd02a4189e23b5ea39561a19dbdf4d36ae10eec
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-06-20 14:28:37 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".navigation");
+
+menu.addEventListener("click", () => {
+  nav.style.width = "60%";
+  nav.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  nav.style.width = 0;
+  nav.style.display = "none";
+});
